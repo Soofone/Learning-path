12db297e875513532bcce2a78c33a6acd06eb184
Soufiane EL HADDADI <elhaddadisoufiane44@gmail.com>
2025-04-24 13:45:56 +0100

    Cpp Workspace

---
+++ Cpp-Area/01-ProblemSolving/Problem#01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+void PrintName(string Name)
+{
+	cout << "Your Name is: " << Name << endl;
+}
+
+int main()
+{
+	PrintName("Soufiane");
+
+	return 0;
+}
